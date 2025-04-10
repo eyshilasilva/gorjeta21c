e63065449d70f2782e909bca8d9d345ae9cff4dd
eyshilasb11 <eyshilasb11@gmail.com>
2025-04-10 00:29:06 +0000

    resetar

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ let bill = 0
  
      function reciveBillValue(){
      bill = document.querySelector("#bill").valueAsNumber
-     
      }
- 
+     function calculateResults(){}
+    
      function receiveOfpeopleValue(){
        numberOfPeople = document.querySelector("#people").valueAsNumber
      }
     
- 
+     function calculateResults(){}
+    
     function receiveTipPercentageValue(value) {
     
       
@@ -23,16 +24,39 @@ let bill = 0
         buttomSelected.classList.add("button-selected")
         
       }
-    
+      function calculateResults(){}
       function receiveCustomTipPercentageValue() {
         tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
         
         removeClassButtomSelected()
-        }
         
+        function calculateResults(){}
         function removeClassButtomSelected() {
          if (buttonSelected !== null) {
           buttonSelected.classList.remove("button-selected")
           buttonSelected = null
       }
       }
+    }
+ 
+ 
+    function calculateResults() {
+      if (bill !== 0 && tipPercentage !== 0 numberOfPeople !== 0) {
+        
+        let tipAmountPerson = calculateTipAmountPerson()
+        calculateResultsPerson()
+        }
+    }
+  
+    function calculateTipAmountPerson() {
+      let tipAmountStrong = document.querySelector(".amount strong")
+      let tipAmountPerson = bill * tipPercentage / number0fPeople
+      tipAmountStrong. textContent = '$${tipAmountPerson.toFixed(2)}'
+    return tipAmountPerson
+    }
+  
+    function  calculateResultsPerson(tipAmountPerson) {
+      let totalStrong = document.querySelector(".total strong")
+      let totalAmountPerson = bill / number0fPeople + tipAmountPerson
+      totalStrong. textContent = '$${totalAmountPerson.toFixed(2)}'
+     }
